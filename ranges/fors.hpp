4623651bbc553955bd2095725f5e32d6e7014fea
Alexey Dmitriev <github@admitriev.name>
2015-02-21 12:09:46 +0300

    Обертки для циклов вперед
     #SPCPPL-43 done

---
+++ ranges/fors.hpp
@@ -0,0 +1,58 @@
+#pragma once
+
+template <typename T>
+class IntegerIterator {
+public:
+	explicit IntegerIterator(int value): value(value) {
+
+	}
+
+	IntegerIterator& operator++ () {
+		++value;
+		return *this;
+	}
+
+	const T operator * () const {
+		return value;
+	}
+
+	bool operator == (IntegerIterator rhs) {
+		return value == rhs.value;
+	}
+
+	bool operator != (IntegerIterator rhs) {
+		return !(*this == rhs);
+	}
+private:
+	T value;
+};
+
+template <typename T>
+class IntegerRange {
+public:
+	IntegerRange(T begin, T end): begin_(begin), end_(end) {
+
+	}
+
+	IntegerIterator<T> begin() const {
+		return IntegerIterator<T>(begin_);
+	}
+
+	IntegerIterator<T> end() const {
+		return IntegerIterator<T>(end_);
+	}
+
+private:
+	T begin_;
+	T end_;
+};
+
+template <typename T>
+IntegerRange<T> range(T to) {
+	return IntegerRange<T>(0, to);
+}
+
+template <typename T>
+IntegerRange<T> range(T from, T to) {
+	return IntegerRange<T>(from, to);
+}
