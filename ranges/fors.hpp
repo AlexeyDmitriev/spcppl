eecdee648b71a258b6505ce826eea2dc988b9c64
Alexey Dmitriev <github@admitriev.name>
2015-02-23 22:58:33 +0300

    Support iterating over ranges downwards
     #SPCPPL-44 done
     #SPCPPL-32 done
     #SPCPPL-30 done

--- ranges/fors.hpp
+++ ranges/fors.hpp
@@ -1,9 +1,25 @@
 #pragma once
 
+#include <iterator>
 #include "../assert.hpp"
 
+/**
+* Support decrementing and multi-passing, but not declared bidirectional(or even forward) because
+* it's reference type is not a reference.
+*
+* It doesn't return reference because
+* 1. Anyway it'll not satisfy requirement [forward.iterators]/6
+*   If a and b are both dereferenceable, then a == b if and only if *a and
+*   b are bound to the same object.
+* 2. It'll not work with reverse_iterator that returns operator * of temporary which is temporary for this iterator
+*
+* Note, reverse_iterator is not guaranteed to work  now too since it works only with bidirectional iterators,
+* but it's seems to work at least on my implementation.
+*
+* It's not really useful anywhere except iterating anyway.
+*/
 template <typename T>
-class IntegerIterator {
+class IntegerIterator : public std::iterator<std::input_iterator_tag, T, std::ptrdiff_t, T*, T> {
 public:
 	explicit IntegerIterator(int value): value(value) {
 
@@ -14,7 +30,24 @@ public:
 		return *this;
 	}
 
-	const T operator * () const {
+	IntegerIterator operator++ (int) {
+		IntegerIterator copy = *this;
+		++value;
+		return copy;
+	}
+
+	IntegerIterator& operator-- () {
+		--value;
+		return *this;
+	}
+
+	IntegerIterator operator-- (int) {
+		IntegerIterator copy = *this;
+		--value;
+		return copy;
+	}
+
+	T operator * () const {
 		return value;
 	}
 
@@ -50,6 +83,27 @@ private:
 };
 
 template <typename T>
+class ReversedIntegerRange {
+	typedef std::reverse_iterator<IntegerIterator<T>> IteratorType;
+public:
+	ReversedIntegerRange(T begin, T end): begin_(begin), end_(end) {
+
+	}
+
+	IteratorType begin() const {
+		return IteratorType(IntegerIterator<T>(begin_));
+	}
+
+	IteratorType end() const {
+		return IteratorType(IntegerIterator<T>(end_));
+	}
+
+private:
+	T begin_;
+	T end_;
+};
+
+template <typename T>
 IntegerRange<T> range(T to) {
 	SPCPPL_ASSERT(to >= 0);
 	return IntegerRange<T>(0, to);
@@ -59,4 +113,16 @@ template <typename T>
 IntegerRange<T> range(T from, T to) {
 	SPCPPL_ASSERT(from <= to);
 	return IntegerRange<T>(from, to);
+}
+
+template <typename T>
+ReversedIntegerRange<T> downrange(T from) {
+	SPCPPL_ASSERT(from >= 0);
+	return ReversedIntegerRange<T>(from, 0);
+}
+
+template <typename T>
+ReversedIntegerRange<T> downrange(T from, T to) {
+	SPCPPL_ASSERT(from >= to);
+	return ReversedIntegerRange<T>(from, to);
 }
