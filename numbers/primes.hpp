cb4a93c0b1a22eaa4f446641847d0aaefaa2679c
Alexey Dmitriev <github@admitriev.name>
2015-02-24 03:03:09 +0300

    Решето эратосфена
     #SPCPPL-18 done

--- numbers/primes.hpp
+++ numbers/primes.hpp
@@ -2,14 +2,43 @@
 
 #include <vector>
 #include <cstddef>
-#include "../identity.hpp"
 
 bool isPrime(int n) {
-	for(int i = 2; i * i <= n; ++i) {
-		if(n % i == 0) {
+	for (int i = 2; i * i <= n; ++i) {
+		if (n % i == 0) {
 			return false;
 		}
 	}
 	return n >= 2;
 }
 
+struct Bool {
+	/* implicit */ Bool(bool value) : value(value) {
+
+	}
+
+	operator bool() const {
+		return value;
+	}
+
+private:
+	bool value;
+};
+
+std::vector<Bool> primeMap(size_t maxN) {
+	std::vector<Bool> prime(maxN + 1, true);
+
+	prime[0] = false;
+	prime[1] = false;
+
+	for (size_t i = 2; i * i <= maxN; ++i) {
+		if (prime[i]) {
+			if (i * 1ULL * i <= maxN) {
+				for (size_t j = i * i; j <= maxN; j += i) {
+					prime[j] = false;
+				}
+			}
+		}
+	}
+	return prime;
+}
