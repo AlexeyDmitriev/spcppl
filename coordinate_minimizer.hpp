22280f1c0629018e3fd5b9292cb122834d6af952
Alexey Dmitriev <github@admitriev.name>
2015-02-28 04:56:48 +0300

    Adds coordinate minimizer
     #SPCCPL-27 done

---
+++ coordinate_minimizer.hpp
@@ -0,0 +1,46 @@
+#include <vector>
+#include "assert.hpp"
+#include "ranges/wrappers.hpp"
+
+template <typename T>
+class CoordinateMinimizer {
+public:
+	CoordinateMinimizer() {
+
+	}
+
+	template <typename R>
+	explicit CoordinateMinimizer(const R& range) : values(range.begin(), range.end()) {
+		finalize();
+	}
+
+	void finalize() {
+		sort(values);
+		#ifdef SPCPPL_DEBUG
+			addingFinished = true;
+		#endif
+	}
+
+	void add(const T& t) {
+		SPCPPL_ASSERT(!addingFinished);
+		values.push_back(t);
+	}
+
+	void add(T&& t) {
+		SPCPPL_ASSERT(!addingFinished);
+		values.push_back(std::move(t));
+	}
+
+	std::size_t find(const T& t) {
+		SPCPPL_ASSERT(addingFinised);
+		auto iterator = lower_bound(values, t);
+		SPCPPL_ASSERT(iterator != values.end() && *iterator == t);
+		return iterator - values.begin();
+	}
+
+private:
+	std::vector<T> values;
+	#ifdef SPCPPL_DEBUG
+		boolean addingFinished;
+	#endif
+};
