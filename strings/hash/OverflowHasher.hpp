e7be12741fc018f10dd98fd84b6a1821be0b6d63
Alexey Dmitriev <github@admitriev.name>
2015-02-28 03:48:23 +0300

    overflow int 64 hasher for strings
     #SPCPPL-16 done

---
+++ strings/hash/OverflowHasher.hpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include "../../ranges/fors.hpp"
+
+class OverflowHasher {
+public:
+	static const unsigned long long multiplier = 97;
+
+	OverflowHasher(const std::string& s): hashes(s.size() + 1) {
+		ensureLength(s.length());
+		for (std::size_t i: range(s.size())) {
+			hashes[i + 1] = hashes[i] * multiplier + s[i];
+		}
+	}
+
+	unsigned long long hash(std::size_t from, std::size_t to) {
+		SPCPPL_ASSERT(from <= to);
+		return hashes[to] - hashes[from] * powers[to - from];
+	}
+
+private:
+
+	static void ensureLength(std::size_t length) {
+		powers.reserve(length + 1);
+		while (powers.size() < length + 1) {
+			powers.push_back(powers.back() * multiplier);
+		}
+	}
+	static std::vector<unsigned long long> powers;
+	std::vector<unsigned long long> hashes;
+};
+
+std::vector<unsigned long long> OverflowHasher::powers = {1};
---
+++ strings/hash/StringHash.hpp
@@ -0,0 +1,28 @@
+#include <cstddef>
+#include <string>
+
+template <typename Hasher>
+class StringHash {
+public:
+	StringHash(const std::string& s): hasher(s) {
+
+	}
+
+	unsigned long long hash(std::size_t from, std::size_t to) {
+		return hasher.hash(from, to);
+	}
+
+	unsigned long long prefixHash(std::size_t to) {
+		return hasher.hash(0, to);
+	}
+
+	unsigned long long suffixHash(std::size_t from) {
+		return hasher.hash(from, hasher.size());
+	}
+
+	unsigned long long hash() {
+		return hasher.hash(0, hasher.size());
+	}
+private:
+	Hasher hasher;
+};
