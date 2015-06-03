c51c260fe66e04d0db5abefbec5b78365e7126b6
Alexey Dmitriev <github@admitriev.name>
2015-06-03 16:01:48 +0300

    Получение всех делителей числа.
     #SPCPPL-70 Done

---
+++ numbers/divisors.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <vector>
+#include "../ranges/wrappers.hpp"
+
+template <typename T>
+std::vector<T> unsortedDivisors(const T& n) {
+	std::vector<T> result;
+	for (int i = 1; i * i <= n; ++i) {
+		if (n % i == 0) {
+			result.push_back(i);
+			if(i * i != n) {
+				result.push_back(n / i);
+			}
+		}
+	}
+	return result;
+}
+
+template <typename T>
+std::vector<T> sortedDivisors(const T& n) {
+	return sorted(unsortedDivisors(n));
+}
