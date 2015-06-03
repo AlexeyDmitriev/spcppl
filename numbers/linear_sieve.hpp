f8c0be02bde6e3d3803768017f5f06b5db19c984
Alexey Dmitriev <github@admitriev.name>
2015-06-03 17:15:40 +0300

    Linear sieve
     #SPCPPL-47 Done

---
+++ numbers/linear_sieve.hpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include "../assert.hpp"
+
+std::vector<int> linearSieve(int maxN) {
+	SPCPPL_ASSERT(maxN > 0);
+	std::vector<int> leastPrimes(maxN + 1);
+	std::vector<int> primes;
+
+	for (int i = 2; i <= maxN; ++i) {
+		if (leastPrimes[i] == 0) {
+			leastPrimes[i] = i;
+			primes.push_back(i);
+		}
+		for (int prime: primes) {
+			if(i * prime > maxN)
+				break;
+			leastPrimes[i * prime] = prime;
+		}
+	}
+	return leastPrimes;
+}
