81d5476f665de344c01ecd369ee068549fc632c1
Alexey Dmitriev <github@admitriev.name>
2015-06-14 02:39:45 +0300

    Divisions with different type of rounding
     #SPCPPL-78 Done

---
+++ numbers/divisions.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "../assert.hpp"
+
+template <typename T>
+T divideCeil(T a, T b) {
+	SPCPPL_ASSERT(b != 0);
+	if (b < 0) {
+		a = -a;
+		b = -b;
+	}
+	return a / b + (a % b > 0);
+}
+
+template <typename T>
+T divideFloor(T a, T b) {
+	SPCPPL_ASSERT(b != 0);
+	if (b < 0) {
+		a = -a;
+		b = -b;
+	}
+	return a / b - (a % b < 0);
+}
+
+template <typename T>
+T divideTowardsZero(T a, T b) {
+	SPCPPL_ASSERT(b != 0);
+	return a / b;
+}
+
+template <typename T>
+T divideAwayFrowZero(T a, T b) {
+	SPCPPL_ASSERT(b != 0);
+	bool changeSign = false;
+	if(a < 0) {
+		changeSign = !changeSign;
+		a = -a;
+	}
+	if(b < 0) {
+		changeSign = !changeSign;
+		b = -b;
+	}
+	int res = (a + b - 1) / b;
+	if(changeSign) {
+		res *= -1;
+	}
+	return res;
+
+}
+
