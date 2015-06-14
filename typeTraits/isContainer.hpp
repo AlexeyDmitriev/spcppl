64009b3eb5b8da4378a8e702870be27d1fc7bf9c
Alexey Dmitriev <github@admitriev.name>
2015-06-15 02:16:39 +0300

    Trait to check if type is collection.
     #SPCPPL-85 Done

---
+++ typeTraits/isContainer.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iterator>
+#include <utility>
+
+template <typename T>
+constexpr auto hasBegin(int) -> decltype(std::begin(std::declval<T>()), true) {
+	return true;
+}
+
+template <typename T>
+constexpr bool hasBegin(...) {
+	return false;
+}
+
+template <typename T>
+struct isContainer : std::integral_constant<bool, hasBegin<T>(0)> {
+};
