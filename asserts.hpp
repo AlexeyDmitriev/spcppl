43d533b89f1788f382eaec839ad650bc6c92e43f
Alexey Dmitriev <github@admitriev.name>
2015-06-17 04:44:15 +0300

    Asserts for different verdicts
     #SPCPPL-90 Done

---
+++ asserts.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <cstdlib>
+
+#ifndef SPCPPL_IGNORE_ASSERTS
+	void assert_re(bool value) {
+		if(!value) {
+			std::exit(1);
+		}
+	}
+
+	void assert_tl(bool value) {
+		if(!value) {
+			while (true);
+		}
+	}
+
+	void assert_wa(bool value) {
+		if(!value) {
+			std::cout << "karamba";
+			std::exit(0);
+		}
+	}
+
+#else
+	#define assert_re(x)
+	#define assert_tl(x)
+	#define assert_wa(x)
+#endif
