4acff80454321f936cc79e3f27cb31d2b93fb281
Alexey Dmitriev <github@admitriev.name>
2015-07-05 01:31:22 +0300

    SUm of geometric progreesion
     #SPCPPL-92 Done

---
+++ numbers/progression.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "../binpow.hpp"
+
+template <typename T>
+T geometricProgrssionSum(T first, T q, long long n) {
+	if (q == 1) {
+		return first * n;
+	}
+	return first * (1 - binpow(q, n)) / (1 - q);
+}
+
+template <typename T>
+T inifiniteGeometricProgressionSum(T first, T q) {
+	SPCPPL_ASSERT(-1 < q && q < 1);
+	return first / (1 - q);
+}
