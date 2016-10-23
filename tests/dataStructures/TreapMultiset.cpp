66e261af9548cba3acafcac092caaeeadd062177
Alexey Dmitriev <github@admitriev.name>
2016-10-24 02:50:12 +0300

    Test noncopiable types in TreapMultiset

--- tests/dataStructures/TreapMultiset.cpp
+++ tests/dataStructures/TreapMultiset.cpp
@@ -2,29 +2,40 @@
 #include <gtest/gtest.h>
 
 TEST(TreapMultisetTest, IndexingInSortedOrder) {
-	TreapMultiset<int> tree;
-	tree.insert(3);
-	tree.insert(7);
-	tree.insert(1);
+	TreapMultiset<int> set;
+	set.insert(3);
+	set.insert(7);
+	set.insert(1);
 
-	ASSERT_EQ(tree.size(), static_cast<std::size_t>(3));
+	ASSERT_EQ(set.size(), static_cast<std::size_t>(3));
 
-	EXPECT_EQ(tree[0], 1);
-	EXPECT_EQ(tree[1], 3);
-	EXPECT_EQ(tree[2], 7);
+	EXPECT_EQ(set[0], 1);
+	EXPECT_EQ(set[1], 3);
+	EXPECT_EQ(set[2], 7);
 }
 
 TEST(TreapMultisetTest, HandlesSameArgumentsCorrectly) {
-	TreapMultiset<int> tree;
-	tree.insert(5);
-	tree.insert(5);
-	tree.insert(1);
+	TreapMultiset<int> set;
+	set.insert(5);
+	set.insert(5);
+	set.insert(1);
 
-	EXPECT_EQ(tree.size(), static_cast<std::size_t>(3));
+	EXPECT_EQ(set.size(), static_cast<std::size_t>(3));
 
-	tree.erase_one(5);
+	set.erase_one(5);
 
-	ASSERT_EQ(tree.size(), static_cast<std::size_t>(2));
-	EXPECT_EQ(tree[0], 1);
-	EXPECT_EQ(tree[1], 5);
+	ASSERT_EQ(set.size(), static_cast<std::size_t>(2));
+	EXPECT_EQ(set[0], 1);
+	EXPECT_EQ(set[1], 5);
+}
+
+TEST(TreapMultisetTest, HandlesMoveOnlyType) {
+	TreapMultiset<std::unique_ptr<int>> set;
+	set.insert(std::make_unique<int>(5));
+	EXPECT_EQ(*set[0], 5);
+}
+
+TEST(TreapMultisetTest, HandlesImmovableType) {
+	TreapMultiset<std::atomic<int>> set;
+	set.emplace();
 }
