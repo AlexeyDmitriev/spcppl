22918cd09e12eede9f2200d1df0a391c53931779
Alexey Dmitriev <github@admitriev.name>
2017-02-13 01:01:26 +0300

    NDArray

---
+++ spcppl/dataStructures/NDArray.h
@@ -0,0 +1,167 @@
+#pragma once
+
+#include <cstddef>
+#include <array>
+#include <vector>
+#include <type_traits>
+#include <spcppl/assert.hpp>
+#include <spcppl/ranges/fors.hpp>
+
+template <typename T, std::size_t depth>
+class NDArrayView {
+public:
+	using Ref = std::conditional_t<depth == 1, T&, NDArrayView<T, depth - 1>>;
+
+	Ref operator[] (std::size_t index) const {
+		return get_element_impl(index);
+	}
+
+	Ref front() const {
+		return get_at_index(0);
+	}
+
+	Ref back() const {
+		if (suffixSizes[0] == 0) {
+			return front();
+		}
+		return get_at_index(suffixSizes[0] - (depth == 1 ? 1 : suffixSizes[1]));
+	}
+
+	operator NDArrayView<const T, depth>() const {
+		return NDArrayView<const T, depth>(elements, suffixSizes);
+	}
+	NDArrayView(NDArrayView&&) = default;
+	NDArrayView& operator= (const NDArrayView&) = delete;
+	NDArrayView&& operator= (NDArrayView&&) = delete;
+
+private:
+	template <size_t d = depth, std::enable_if_t<d == 1>* = nullptr>
+	T& get_element_impl(std::size_t index) const {
+		return elements[index];
+	}
+
+	template <size_t d = depth, std::enable_if_t<d != 1>* = nullptr>
+	NDArrayView<T, depth - 1> get_element_impl(std::size_t index) const {
+		return NDArrayView<T, depth - 1>(elements + suffixSizes[1] * index, suffixSizes + 1);
+	}
+
+	template <size_t d = depth, std::enable_if_t<d == 1>* = nullptr>
+	T& get_at_index(std::size_t index) const {
+		return elements[index];
+	}
+
+	template <size_t d = depth, std::enable_if_t<d != 1>* = nullptr>
+	NDArrayView<T, depth - 1> get_at_index(std::size_t index) const {
+		return NDArrayView<T, depth - 1>(elements + index, suffixSizes + 1);
+	}
+
+	NDArrayView(T* elements, const std::size_t* suffixSizes):
+			elements(elements),
+			suffixSizes(suffixSizes) {
+	}
+	T* elements;
+	const std::size_t* suffixSizes;
+
+	template <typename U, std::size_t d>
+	friend class NDArray;
+
+	template <typename U, std::size_t d>
+	friend class NDArrayView;
+
+	template <typename U, std::size_t d>
+	friend void assignView(NDArrayView<U, d> lhs, NDArrayView<U, d> rhs);
+};
+
+template <typename T, std::size_t depth>
+void assignView(NDArrayView<T, depth> lhs, NDArrayView<T, depth> rhs) {
+	SPCPPL_ASSERT(lhs.suffixSizes[0] == rhs.suffixSizes[0]);
+	std::copy(rhs.elements, rhs.elements + lhs.suffixSizes[0], lhs.elements);
+};
+
+template <typename T, std::size_t d>
+bool operator==(const NDArrayView<T, d>&, const NDArrayView<T, d>&) = delete;
+
+template <typename T, std::size_t d>
+bool operator!=(const NDArrayView<T, d>&, const NDArrayView<T, d>&) = delete;
+
+template <typename T, std::size_t depth>
+using NDArrayConstView = NDArrayView<const T, depth>;
+
+template <typename T, std::size_t depth>
+class NDArray {
+public:
+	using Ref = typename NDArrayView<T, depth>::Ref;
+
+	explicit NDArray(const std::array<std::size_t, depth>& sizes):
+			suffixSizes(suffixProducts(sizes)),
+			elements(suffixSizes[0]) {
+	}
+
+	NDArray(const std::array<std::size_t, depth>& sizes, const T& t):
+			suffixSizes(suffixProducts(sizes)),
+			elements(suffixSizes[0], t) {
+	}
+
+	Ref operator[] (std::size_t index) {
+		return static_cast<NDArrayView<T, depth>>(*this)[index];
+	}
+
+	Ref operator[] (std::size_t index) const {
+		return static_cast<NDArrayView<const T, depth>>(*this)[index];
+	}
+
+	operator NDArrayView<T, depth>() {
+		return NDArrayView<T, depth>(elements.data(), suffixSizes.data());
+	}
+
+	operator NDArrayConstView<T, depth>() const {
+		return NDArrayConstView<T, depth>(elements.data(), suffixSizes.data());
+	}
+
+	Ref front() {
+		return static_cast<NDArrayView<T, depth>>(*this).front();
+	}
+
+	Ref front() const {
+		return static_cast<NDArrayConstView<T, depth - 1>>(*this).front();
+	}
+
+	Ref back() {
+		return static_cast<NDArrayView<T, depth>>(*this).back();
+	}
+
+	Ref back() const {
+		return static_cast<NDArrayConstView<T, depth - 1>>(*this).back();
+	}
+private:
+	static std::array<std::size_t, depth> suffixProducts(const std::array<std::size_t, depth>& sizes) {
+		std::array<std::size_t, depth> result;
+		std::size_t last = 1;
+		for (std::size_t index: downrange(depth)) {
+			last *= sizes[index];
+			result[index] = last;
+		}
+		return result;
+	}
+	std::array<std::size_t, depth> suffixSizes;
+	std::vector<T> elements;
+};
+
+template <typename T, typename... Args>
+NDArray<T, sizeof...(Args)> makeArray(Args... sizes) {
+	return NDArray<T, sizeof...(Args)>(
+			std::array<std::size_t, sizeof...(Args)>{{
+					static_cast<std::size_t>(sizes)...
+			}}
+	);
+}
+
+template <typename T, typename... Args>
+NDArray<T, sizeof...(Args)> makeFilledArray(const T& value, Args... sizes) {
+	return NDArray<T, sizeof...(Args)>(
+			std::array<std::size_t, sizeof...(Args)>{{
+					static_cast<std::size_t>(sizes)...
+			}},
+			value
+	);
+}
---
+++ tests/dataStructures/NDArray.cpp
@@ -0,0 +1,57 @@
+#include <gtest/gtest.h>
+#include <spcppl/dataStructures/NDArray.h>
+
+TEST(NDArrayTest, Creation) {
+	NDArray<int, 2> array({{3, 5}}, 0);
+	EXPECT_EQ(array[0][1], 0);
+}
+
+TEST(NDArrayTest, Assignment) {
+	NDArray<int, 2> array({{3, 5}}, 0);
+	EXPECT_EQ(array[2][1], 0);
+
+	array[2][1] = 1;
+
+	EXPECT_EQ(array[0][0], 0);
+	EXPECT_EQ(array[2][1], 1);
+}
+
+TEST(NDArrayTest, FrontBackTest) {
+	NDArray<int, 3> array({{3, 5, 7}}, 1);
+	array.back().front().back() = 5;
+	EXPECT_EQ(array[2][0][6], 5);
+
+	EXPECT_EQ(&array.back().front().back(), &array[2][0][6]);
+
+	EXPECT_EQ(&array[1].back()[4], &array[1][4][4]);
+}
+
+TEST(NDArrayTest, AssignViewTest) {
+	NDArray<int, 2> array({{2, 3}}, 0);
+	array[0][0] = 1;
+	array[0][1] = 2;
+	array[1][0] = 3;
+	array[1][1] = 4;
+	array[2][0] = 5;
+	array[2][1] = 6;
+
+	assignView(array[2], array[1]);
+
+	EXPECT_EQ(array[2][0], 3);
+	EXPECT_EQ(array[2][1], 4);
+	EXPECT_EQ(array[0][0], 1);
+	EXPECT_EQ(array[0][1], 2);
+	EXPECT_EQ(array[1][0], 3);
+	EXPECT_EQ(array[1][1], 4);
+}
+
+TEST(NDArrayTest, CreateArrayTest) {
+	auto array = makeArray<int>(5, 7, 2);
+	EXPECT_EQ(array[3][2][1], 0);
+	EXPECT_EQ(&array.back().back().back() - &array.front().front().front(), 5 * 7 * 2 - 1);
+
+	auto filledArray = makeFilledArray<int>(42, 3, 2, 8);
+
+	EXPECT_EQ(filledArray[1][1][1], 42);
+	EXPECT_EQ(&filledArray.back().back().back() - &filledArray.front().front().front(), 3 * 2 * 8 - 1);
+}
